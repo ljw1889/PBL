a9af45b9bbec15d5d98ca71e7e9f67ca78dad976
ljw1889 <ljw1889@gmail.com>
2021-04-25 22:12:57 +0900

    add #1

--- PBL/스택 문제.c
+++ PBL/스택 문제.c
@@ -1,35 +1,61 @@
 ﻿#include <stdio.h>
-#define MAX 100000
+#include <string.h>
 
-int stack[MAX];
-int top = -1;
+int main()
+{
+    int N;
+    int cmd;
 
-void push(int data) {      //top에 data를 넣고 top의 값을 1증가
-    if (top >= MAX - 1)
-        return;
-    stack[++top] = data;
-}
+    int stack[10000];
+    int i;
+    int top;
+    char input[5];
+    scanf("%d", &N); //N회 명령어 시행
+    top = 0;
 
-void pop() {           // top의 값에 0을 넣는다 즉, 앞의 top값을 지운다는 의미와같다.이후에 top의 값을 1 줄인다.
+    for (i = 0; i < N; i++) {
 
-    if (top < 0)
-        return;
-    stack[top--] = 0;
-}
+        scanf("%s", &input);
 
-int main(void) {
-    int k;
-    int data, sum = 0;
-    scanf("%d", &k); //k회 시행
-    while (k--) {
-        scanf("%d", &data); //값을 입력하고 0이 아니면 push 0이면 pop을 실행
-        if (data == 0)
-            pop();
-        else
-            push(data);
+        if (strcmp(input, "push") == 0) {
+            scanf("%d", &cmd);              //입력한 cmd 값을 stack의 top에 넣는다.그 후 top의 값을 1 늘린다.
+
+            stack[top] = cmd;
+            top++;
+        }
+
+        else if (strcmp(input, "pop") == 0) {  //top이 가리키는 값을 하나 빼고 top의 값을 1 내린다.만약 top이 0이면 - 1을 출력한다.
+
+            if (top == 0) {
+                printf("%d\n", -1);
+            }
+
+            else {
+                printf("%d\n", stack[top - 1]);
+                top--;
+            }
+        }
+
+        else if (strcmp(input, "size") == 0) { //top을 출력한다.
+            printf("%d\n", top);
+        }
+
+        else if (strcmp(input, "top") == 0) { //top이 0일 경우 –1을 출력하고 아니면 stack[top-1]의 값을 출력한다.
+            if (top <= 0) {
+                printf("%d\n", -1);
+            }
+            else {
+                printf("%d\n", stack[top - 1]);
+            }
+        }
+
+        else if (strcmp(input, "empty") == 0) {  //top이 0이면 1을 출력하고 아니면 0을 출력한다.      
+            if (top == 0) {
+                printf("%d\n", 1);
+            }
+            else {
+                printf("%d\n", 0);
+            }
+        }
     }
-    for (int i = 0; i <= top; i++) {  //stack 안 의 값을 더한다.
-        sum += stack[i];
-    }
-    printf("%d\n", sum);
 }
